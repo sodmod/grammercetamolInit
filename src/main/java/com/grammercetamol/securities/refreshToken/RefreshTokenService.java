6a07b8cfee7dbdb0be3de3bd40cbccd1d859fdc9
sodmod <horluwatosin1999@gmail.com>
2023-03-27 10:20:26 +0100

    RefreshToken

--- src/main/java/com/grammercetamol/securities/refreshToken/RefreshTokenService.java
+++ src/main/java/com/grammercetamol/securities/refreshToken/RefreshTokenService.java
@@ -3,14 +3,13 @@ package com.grammercetamol.securities.refreshToken;
 import com.grammercetamol.utilities.repositories.UsersRepositories;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Service;
 
 import java.time.Instant;
 import java.util.UUID;
 
 @Service
-public class RefreshTokenService implements CommandLineRunner {
+public class RefreshTokenService {
     @Autowired
     private UsersRepositories usersRepositories;
     @Autowired
@@ -25,11 +24,5 @@ public class RefreshTokenService implements CommandLineRunner {
         refreshToken.setExpireDate(Instant.now().plusMillis(refreshTokenExpired));
 //        refreshTokenRepo.save(refreshToken);
         return refreshToken;
-    }
-
-    @Override
-    public void run(String... args) throws Exception {
-        RefreshToken c = createRefreshToken(1L);
-        System.out.println(c);
     }
 }
